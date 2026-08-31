4f1bfd1a69c6c0c704cf14063f47c94f5ccf459e
aleem-anwar <aleemanwar245@gmail.com>
2026-08-31 15:57:43 +0530

    Update attendance_app.py

--- attendance_app.py
+++ attendance_app.py
@@ -3,7 +3,7 @@ import datetime
 import math
 import pandas as pd
 
-st.set_page_config(page_title="IIITP Attendance Portal", layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="IIITP Attendance Tracker for CSE(SEC-A)", layout="wide", initial_sidebar_state="collapsed")
 
 # ==========================================
 # KINETIC TYPOGRAPHY DESIGN SYSTEM CSS
